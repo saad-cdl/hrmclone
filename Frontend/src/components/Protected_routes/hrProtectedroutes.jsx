ff38039ff02fc56f3ab4767e5e3b0d42f0a1ebf1
Saad Ahmad Khan <cto@codexterlabs.com>
2025-06-01 08:47:26 -0400

    Update hrProtectedroutes.jsx

--- Frontend/src/components/Protected_routes/hrProtectedroutes.jsx
+++ Frontend/src/components/Protected_routes/hrProtectedroutes.jsx
@@ -1,23 +1,9 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import useAuth from '../hooks/useAuth';  // Import the custom hook
 
 const HrProtectedroutes = ({ component: Component }) => {
-  const { isloggedIn, admin, isLoading } = useAuth();
-
-  if (isLoading) {
-    return <div>Loading...</div>; // Show a loading state while authentication is being checked
-  }
-  console.log(isloggedIn, admin);
-  if (isloggedIn && admin) {
+    // Always render the component without authentication check
     return <Component />;
-  } else if (isloggedIn && !admin) {
-    toast.error("Admin access only");
-    return <Navigate to="/emp-dboard" />;
-  } else {
-    return <Navigate to="/login" />;
-  }
 };
 
 export default HrProtectedroutes;
