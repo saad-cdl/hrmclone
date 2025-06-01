b5d51f4c90ebb960fa1ad1411c5e9485408b09ef
Saad Ahmad Khan <cto@codexterlabs.com>
2025-06-01 08:26:45 -0400

    vercel changes

--- Backend/src/utils/AsyncHandler.js
+++
@@ -1,8 +0,0 @@
-const asynchandler = (reuqesthandler) => {
-  return (req, res, next) => {
-    Promise.resolve(reuqesthandler(req, res, next)).catch((error) => {
-      next(error);
-    });
-  };
-};
-export default asynchandler;
